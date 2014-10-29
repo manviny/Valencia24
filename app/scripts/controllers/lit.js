6ce70366b0d0111bba73825e1a0f2391369fd603
manviny <manol@manol.home>
2014-10-29 07:28:11 +0100

    audio auto

--- app/scripts/controllers/lit.js
+++ app/scripts/controllers/lit.js
@@ -8,29 +8,52 @@
  * Controller of the valencia24App
  */
 angular.module('valencia24App')
-  .controller('LitCtrl', function ($scope, $cordovaMedia) {
+  .controller('LitCtrl', function ($scope, $cordovaMedia, cordova) {
    
-    $scope.path = path;																// path to thumbs, audios.. 
-
-  	$scope.lit =  valencia.lits[myNavigator.getCurrentPage().options.litIndex];		// datos del lit actual
-
-  	// variables audio limpias
-    var my_media = null;
-    var mediaTimer = null;
-
-    // carga el audio y play
-    console.debug("",$scope.path.audios + $scope.lit.audio);
+    $scope.path = path;																                              // path to thumbs, audios.. 
+  	$scope.lit =  valencia.lits[myNavigator.getCurrentPage().options.litIndex];		  // datos del lit actual
 
 
-    var src =  $scope.path.audios + $scope.lit.audio;
+    // Crea objecto de audio
+    try {
+        var mediaSource = $cordovaMedia.newMedia($scope.path.audios + $scope.lit.media + '.mp3');
+        var promise = mediaSource.promise;
+        var mediaStatus = mediaSource.mediaStatus;
+        var media = mediaSource.media;
+        $cordovaMedia.play(media);
+      
+        // $cordovaMedia.play(media); 
 
-    var mediaSource = $cordovaMedia.newMedia(src);
-    var promise = mediaSource.promise;
-    var mediaStatus = mediaSource.mediaStatus;
-    var media = mediaSource.media;
+    } catch (exception) {
 
-    $cordovaMedia.play(media);
+        // this code handles exceptions
+        console.debug("No es un telefono, volvemos pq no puede ejecutar audio","salir");
+
+    } finally {
+        // this code always gets executed
+    }
 
 
-console.debug("audio", $scope.path.audios + $scope.lit.audio);
+    //detecta una vez que hemos vuelto a la pagina que llama
+    myNavigator.once("postpop", function(event){
+      console.debug("event",event);
+      $cordovaMedia.release(media);
+    });
+
+    //detecta una vez que hemos cambiado a otra pagina
+    tabbar.on('prechange', function(event) {
+      $cordovaMedia.release(media);
+      console.debug("event",event);
+      // event.cancel();
+    });
+
+
+
+    $scope.play = function(){$cordovaMedia.play(media); }
+    $scope.pause = function(){$cordovaMedia.pause(media);}
+    $scope.stop = function(){$cordovaMedia.stop(media);}
+
+
+
+ 
   });
--- app/scripts/controllers/portada.js
+++ app/scripts/controllers/portada.js
@@ -8,16 +8,18 @@
  * Controller of the valencia24App
  */
 angular.module('valencia24App')
-  .controller('PortadaCtrl', function ($scope, cordova, $cordovaGeolocation) {
+  .controller('PortadaCtrl', function ($scope, cordova, $cordovaGeolocation, $cordovaSpinnerDialog) {
 
+  		 
+  	// mi posicion GPS
 	  cordova.ready.then(function () {
-
-	    alert('Cordova is ready');
+	  	 $cordovaSpinnerDialog.show("Buscando posición","...");
 	        $cordovaGeolocation
 		    .getCurrentPosition()
 		    .then(function (position) {
-		      var lat  = position.coords.latitude
-		      var long = position.coords.longitude
+		      var lat  = position.coords.latitude;
+		      var long = position.coords.longitude;
+		      $cordovaSpinnerDialog.hide();
 		      alert(lat + '  ' + long);
 		    }, function(err) {
 		    	alert('error');
@@ -31,7 +33,7 @@ angular.module('valencia24App')
     $scope.lits = valencia.lits
 
   	$scope.goLit = function(index){
-  		myNavigator.pushPage('views/lit.html',{litIndex:index})
+  		myNavigator.pushPage('views/lit.html',{litIndex:index, animation: "fade"})
   	}  
 
   });
